124e2bae01e40b0adfab63c8a73e3654492bb9bd
Teo Blomqvist <teoblomqvist@gmail.com>
2024-11-16 21:13:16 +0200

    added reminders to add logic intended for enabling admins to delete places and reviews

--- routes/admin.py
+++ routes/admin.py
@@ -3,3 +3,13 @@
 def add_place():
     # Logic to add a new place
     pass
+
+def remove_place():
+    # Logic to remove a place
+    pass
+
+def remove_review():
+    # Logic to remove a review
+    pass
+
+
